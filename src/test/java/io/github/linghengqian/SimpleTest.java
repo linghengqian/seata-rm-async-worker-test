fa46baefebf3c936959d302434251eece0b0641d
linghengqian <linghengqian@outlook.com>
2025-01-20 16:00:39 +0800

    another test

--- src/test/java/io/github/linghengqian/SimpleTest.java
+++ src/test/java/io/github/linghengqian/SimpleTest.java
@@ -9,112 +9,71 @@ import org.apache.seata.rm.RMClient;
 import org.apache.seata.rm.datasource.DataSourceProxy;
 import org.apache.seata.tm.TMClient;
 import org.awaitility.Awaitility;
-import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.time.Duration;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 
-@SuppressWarnings("SqlNoDataSourceInspection")
 public class SimpleTest {
 
     @Test
     void test() {
         assertThat(System.getProperty("service.default.grouplist"), is(nullValue()));
-        try (GenericContainer<?> seataContainer = new GenericContainer<>("apache/seata-server:2.2.0")) {
-            seataContainer.withExposedPorts(7091, 8091).waitingFor(
-                    Wait.forHttp("/health").forPort(7091).forStatusCode(200).forResponsePredicate("ok"::equals)
-            );
-            seataContainer.start();
-            System.setProperty("service.default.grouplist", "127.0.0.1:" + seataContainer.getMappedPort(8091));
-            TMClient.init("test-first", "default_tx_group");
-            RMClient.init("test-first", "default_tx_group");
-            Stream.of("demo_ds_0", "demo_ds_1", "demo_ds_2").parallel().forEach(databaseName -> {
-                try {
-                    exSQL(databaseName);
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-            TmNettyRemotingClient.getInstance().destroy();
-            RmNettyRemotingClient.getInstance().destroy();
-            ConfigurationFactory.reload();
-            System.clearProperty("service.default.grouplist");
-        }
+        CompletableFuture<Void> futureFirst = CompletableFuture.runAsync(this::testWithSeataClient);
+        CompletableFuture<Void> futureSecond = futureFirst.thenAccept(result -> {
+            CompletableFuture<Void> childFuture = CompletableFuture.runAsync(this::testWithSeataClient);
+            Awaitility.await().atMost(5L, TimeUnit.MINUTES).until(childFuture::isDone);
+        });
+        futureSecond.thenAccept(result -> {
+            CompletableFuture<Void> childFuture = CompletableFuture.runAsync(this::testWithoutSeataClient);
+            Awaitility.await().atMost(5L, TimeUnit.MINUTES).until(childFuture::isDone);
+        });
+        Awaitility.await().atMost(5L, TimeUnit.MINUTES).until(futureFirst::isDone);
+        Awaitility.await().atMost(5L, TimeUnit.MINUTES).until(futureSecond::isDone);
     }
 
-
-    private void exSQL(String databaseName) throws SQLException {
+    private void testWithSeataClient() {
+        GenericContainer<?> seataContainer = new GenericContainer<>("apache/seata-server:2.2.0");
+        seataContainer.withExposedPorts(7091, 8091).waitingFor(
+                Wait.forHttp("/health").forPort(7091).forStatusCode(200).forResponsePredicate("ok"::equals)
+        );
+        seataContainer.start();
+        System.setProperty("service.default.grouplist", "127.0.0.1:" + seataContainer.getMappedPort(8091));
+        TMClient.init("test-first", "default_tx_group");
+        RMClient.init("test-first", "default_tx_group");
         HikariConfig config = new HikariConfig();
         config.setDriverClassName("org.testcontainers.jdbc.ContainerDatabaseDriver");
-        config.setJdbcUrl("jdbc:tc:postgresql:17.2-bookworm://test/%s?TC_INITSCRIPT=init.sql".formatted(databaseName));
+        config.setJdbcUrl("jdbc:tc:postgresql:17.2-bookworm://test/%s?TC_INITSCRIPT=init.sql".formatted("demo_ds_0"));
         DataSource hikariDataSource = new HikariDataSource(config);
         DataSource seataDataSource = new DataSourceProxy(hikariDataSource);
         Awaitility.await().atMost(Duration.ofSeconds(15L)).ignoreExceptions().until(() -> {
             seataDataSource.getConnection().close();
             return true;
         });
-        try (Connection connection = seataDataSource.getConnection()) {
-            connection.createStatement().execute("""
-                    CREATE TABLE IF NOT EXISTS t_order (
-                        order_id BIGSERIAL NOT NULL PRIMARY KEY,
-                        order_type INTEGER,
-                        user_id INTEGER NOT NULL,
-                        address_id BIGINT NOT NULL,
-                        status VARCHAR(50)
-                    )
-                    """);
-            connection.createStatement().execute("TRUNCATE TABLE t_order");
-            IntStream.range(1, 100001).parallel().forEach(i -> {
-                try {
-                    connection.createStatement().executeUpdate(
-                            "INSERT INTO t_order (order_id, user_id, order_type, address_id, status) VALUES (%d, %d, %d, %d, 'INSERT_TEST')"
-                                    .formatted(i, i, i % 2, i));
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-            connection.createStatement().executeQuery("SELECT * FROM t_order");
-            IntStream.range(1, 100001).parallel().forEach(i -> {
-                try {
-                    connection.createStatement().executeUpdate("DELETE FROM t_order WHERE order_id=%d".formatted(i));
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-            connection.createStatement().executeQuery("SELECT * FROM t_order");
-        }
-        try (Connection connection = seataDataSource.getConnection()) {
-            try {
-                connection.setAutoCommit(false);
-                connection.createStatement().executeUpdate("INSERT INTO t_order (order_id, user_id, order_type, address_id, status) VALUES (2024, 2024, 0, 2024, 'INSERT_TEST')");
-                connection.createStatement().executeUpdate("INSERT INTO t_order_does_not_exist (test_id_does_not_exist) VALUES (2024)");
-                connection.commit();
-            } catch (final SQLException ignored) {
-                connection.rollback();
-            } finally {
-                connection.setAutoCommit(true);
-            }
-        }
-        try (Connection conn = seataDataSource.getConnection();
-             ResultSet resultSet = conn.createStatement().executeQuery("SELECT * FROM t_order WHERE order_id = 2024")) {
-            assertThat(resultSet.next(), CoreMatchers.is(false));
-        }
-        try (Connection connection = seataDataSource.getConnection();
-             Statement statement = connection.createStatement()) {
-            statement.execute("DROP TABLE IF EXISTS t_order");
-        }
+        TmNettyRemotingClient.getInstance().destroy();
+        RmNettyRemotingClient.getInstance().destroy();
+        ConfigurationFactory.reload();
+        System.clearProperty("service.default.grouplist");
+        seataContainer.close();
+    }
+
+    private void testWithoutSeataClient() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("org.testcontainers.jdbc.ContainerDatabaseDriver");
+        config.setJdbcUrl("jdbc:tc:postgresql:17.2-bookworm://test/%s?TC_INITSCRIPT=init.sql".formatted("demo_ds_1"));
+        HikariDataSource hikariDataSource = new HikariDataSource(config);
+        Awaitility.await().atMost(Duration.ofSeconds(15L)).ignoreExceptions().until(() -> {
+            hikariDataSource.getConnection().close();
+            return true;
+        });
+        hikariDataSource.close();
     }
 }
